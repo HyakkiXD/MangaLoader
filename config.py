2820ae455d3a0a94f84361b8f0e6552b4bb55ac5
Riyan [wade] <86948336+whiteFangx@users.noreply.github.com>
2022-08-31 19:34:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    APP_ID = '7425008'
-    API_HASH = 'c2e856cc2197f8aa52530ac67ffa9d72'
-    BOT_TOKEN = '2055736204:AAHw98MzwsIMZE5edbFoVl3Wgda5TpsqCsM'
-    OWNER_ID = '1811998438'
-    AS_ZIP = bool(os.environ['AS_ZIP'])
+    APP_ID = 7425008
+    API_HASH = "c2e856cc2197f8aa52530ac67ffa9d72"
+    BOT_TOKEN = "2055736204:AAHw98MzwsIMZE5edbFoVl3Wgda5TpsqCsM"
+    OWNER_ID = 1811998438
+    AS_ZIP = "ANYTHING"
